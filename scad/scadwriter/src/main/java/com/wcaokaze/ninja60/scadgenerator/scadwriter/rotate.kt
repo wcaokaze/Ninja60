76bda25cbe1e958754a2ffcf8ad755d694f60f58
wcaokaze <wcaokaze@gmail.com>
2021-03-22 01:18:15 +0900

    rotateに渡す角度はdegreesに変換する必要がありますね

--- scad/scadwriter/src/main/java/com/wcaokaze/ninja60/scadgenerator/scadwriter/rotate.kt
+++ scad/scadwriter/src/main/java/com/wcaokaze/ninja60/scadgenerator/scadwriter/rotate.kt
@@ -1,5 +1,7 @@
 package com.wcaokaze.ninja60.scadgenerator.scadwriter
 
+import java.lang.StrictMath.*
+
 data class Rotate(val x: Double, val y: Double, val z: Double) {
    override fun toString() = "[$x, $y, $z]"
 }
@@ -13,5 +15,9 @@ inline fun ScadWriter.rotate(x: Double = 0.0,
 }
 
 inline fun ScadWriter.rotate(rotate: Rotate, children: ScadWriter.() -> Unit) {
-   writeBlock("rotate($rotate)", children)
+   val x = toDegrees(rotate.x)
+   val y = toDegrees(rotate.y)
+   val z = toDegrees(rotate.z)
+
+   writeBlock("rotate([$x, $y, $z])", children)
 }
