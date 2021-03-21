93ad82e2a1de5897f03ac3c4ab1eb8e49072770a
wcaokaze <wcaokaze@gmail.com>
2021-03-22 01:15:20 +0900

    linearExtrude => linear_extrude

--- scad/scadwriter/src/main/java/com/wcaokaze/ninja60/scadgenerator/scadwriter/linearExtrude.kt
+++ scad/scadwriter/src/main/java/com/wcaokaze/ninja60/scadgenerator/scadwriter/linearExtrude.kt
@@ -1,11 +1,11 @@
 package com.wcaokaze.ninja60.scadgenerator.scadwriter
 
 inline fun ScadWriter.linearExtrude(height: Double, children: ScadWriter.() -> Unit) {
-   writeBlock("linearExtrude($height)", children)
+   writeBlock("linear_extrude($height)", children)
 }
 
 inline fun ScadWriter.linearExtrude(height: Double, center: Boolean,
                                     children: ScadWriter.() -> Unit)
 {
-   writeBlock("linearExtrude($height, center = $center)", children)
+   writeBlock("linear_extrude($height, center = $center)", children)
 }
