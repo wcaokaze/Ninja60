f4229d489359620ab6dc4e292ae1f80754784571
wcaokaze <wcaokaze@gmail.com>
2021-03-22 02:07:33 +0900

    SizeとAngleに各種演算子オーバーロードを実装

--- scad/scadwriter/src/main/java/com/wcaokaze/ninja60/scadgenerator/scadwriter/foundation/Angle.kt
+++ scad/scadwriter/src/main/java/com/wcaokaze/ninja60/scadgenerator/scadwriter/foundation/Angle.kt
@@ -2,9 +2,27 @@ package com.wcaokaze.ninja60.scadgenerator.scadwriter.foundation
 
 import java.lang.StrictMath.*
 
-inline class Angle(private val a: Double) {
+inline class Angle(private val a: Double) : Comparable<Angle> {
    override fun toString() = toDegrees(a).toString()
+
+   operator fun plus (another: Angle) = Angle(a + another.a)
+   operator fun minus(another: Angle) = Angle(a - another.a)
+
+   operator fun times(n: Int)    = Angle(a * n)
+   operator fun times(n: Double) = Angle(a * n)
+   operator fun div  (n: Int)    = Angle(a / n)
+   operator fun div  (n: Double) = Angle(a / n)
+
+   override operator fun compareTo(other: Angle): Int = a.compareTo(other.a)
+
+   operator fun unaryMinus() = Angle(-a)
+   operator fun unaryPlus () = Angle(+a)
+
+   operator fun rangeTo(end: Angle) = AngleRange(this, end)
 }
+
+data class AngleRange(override val start: Angle,
+                      override val endInclusive: Angle) : ClosedRange<Angle>
 
 inline val Double.rad get() = Angle(this)
 
--- scad/scadwriter/src/main/java/com/wcaokaze/ninja60/scadgenerator/scadwriter/foundation/Size.kt
+++ scad/scadwriter/src/main/java/com/wcaokaze/ninja60/scadgenerator/scadwriter/foundation/Size.kt
@@ -1,8 +1,26 @@
 package com.wcaokaze.ninja60.scadgenerator.scadwriter.foundation
 
-inline class Size(private val s: Double) {
+inline class Size(private val s: Double) : Comparable<Size> {
    override fun toString() = s.toString()
+
+   operator fun plus (another: Size) = Size(s + another.s)
+   operator fun minus(another: Size) = Size(s - another.s)
+
+   operator fun times(n: Int)    = Size(s * n)
+   operator fun times(n: Double) = Size(s * n)
+   operator fun div  (n: Int)    = Size(s / n)
+   operator fun div  (n: Double) = Size(s / n)
+
+   override operator fun compareTo(other: Size): Int = s.compareTo(other.s)
+
+   operator fun unaryMinus() = Size(-s)
+   operator fun unaryPlus () = Size(+s)
+
+   operator fun rangeTo(end: Size) = SizeRange(this, end)
 }
+
+data class SizeRange(override val start: Size,
+                     override val endInclusive: Size) : ClosedRange<Size>
 
 inline val Int   .mm get() = Size(this.toDouble())
 inline val Double.mm get() = Size(this)
