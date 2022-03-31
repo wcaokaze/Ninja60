aee6757f9be468cdb55ab6dc4117049eba2055cd
wcaokaze <wcaokaze@gmail.com>
2022-04-01 02:56:53 +0900

    added: Point2dRange, Point3dRange

--- scad/scadwriter/src/main/java/com/wcaokaze/scadwriter/foundation/Point.kt
+++ scad/scadwriter/src/main/java/com/wcaokaze/scadwriter/foundation/Point.kt
@@ -1,5 +1,7 @@
 package com.wcaokaze.scadwriter.foundation
 
+import kotlin.math.*
+
 data class Point(val distanceFromOrigin: Size)
    : ScadValue(), Comparable<Point>
 {
@@ -56,6 +58,33 @@ data class Point2d(val x: Point, val y: Point) : ScadValue() {
    operator fun plus (size: Size2d) = Point2d(x + size.x, y + size.y)
    operator fun minus(size: Size2d) = Point2d(x - size.x, y - size.y)
    operator fun minus(point: Point2d) = Size2d(x - point.x, y - point.y)
+
+   operator fun rangeTo(end: Point2d) = Point2dRange(this, end)
+}
+
+data class Point2dRange(val start: Point2d,
+                        val endInclusive: Point2d)
+{
+   infix fun step(step: Size) = Iterable {
+      object : Iterator<Point2d> {
+         private val precision = step / 16.0
+         private var nextIndex = 0
+
+         private val vector = endInclusive - start
+
+         private val norm = Size(
+            sqrt(vector.x.numberAsMilliMeter * vector.x.numberAsMilliMeter
+               + vector.y.numberAsMilliMeter * vector.y.numberAsMilliMeter)
+         )
+
+         override fun hasNext() = step * nextIndex <= norm + precision
+
+         override fun next(): Point2d {
+            return start + Size2d(vector.x * (step / norm),
+                                  vector.y * (step / norm)) * nextIndex++
+         }
+      }
+   }
 }
 
 data class Point3d(val x: Point, val y: Point, val z: Point) : ScadValue() {
@@ -73,6 +102,35 @@ data class Point3d(val x: Point, val y: Point, val z: Point) : ScadValue() {
    operator fun plus (size: Size3d) = Point3d(x + size.x, y + size.y, z + size.z)
    operator fun minus(size: Size3d) = Point3d(x - size.x, y - size.y, z - size.z)
    operator fun minus(point: Point3d) = Size3d(x - point.x, y - point.y, z - point.z)
+
+   operator fun rangeTo(end: Point3d) = Point3dRange(this, end)
+}
+
+data class Point3dRange(val start: Point3d,
+                        val endInclusive: Point3d)
+{
+   infix fun step(step: Size) = Iterable {
+      object : Iterator<Point3d> {
+         private val precision = step / 16.0
+         private var nextIndex = 0
+
+         private val vector = endInclusive - start
+
+         private val norm = Size(
+            sqrt(vector.x.numberAsMilliMeter * vector.x.numberAsMilliMeter
+               + vector.y.numberAsMilliMeter * vector.y.numberAsMilliMeter
+               + vector.z.numberAsMilliMeter * vector.z.numberAsMilliMeter)
+         )
+
+         override fun hasNext() = step * nextIndex <= norm + precision
+
+         override fun next(): Point3d {
+            return start + Size3d(vector.x * (step / norm),
+                                  vector.y * (step / norm),
+                                  vector.z * (step / norm)) * nextIndex++
+         }
+      }
+   }
 }
 
 fun Iterable<Point>  .average() = Point(map { it.distanceFromOrigin } .average())
