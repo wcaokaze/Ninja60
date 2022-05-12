96f544084735b2d884d46e9e9e26f0957a0c20d6
wcaokaze <wcaokaze@gmail.com>
2022-05-13 02:09:38 +0900

    backRotaryEncoderKnobを取り付ける柱を支える壁をつけた

--- scad/app/case/src/main/java/com/wcaokaze/ninja60/case/scad/backRotaryEncoderGearHolder.kt
+++ scad/app/case/src/main/java/com/wcaokaze/ninja60/case/scad/backRotaryEncoderGearHolder.kt
@@ -10,6 +10,15 @@ import com.wcaokaze.scadwriter.foundation.*
 internal fun ScadParentObject.backRotaryEncoderGearHolder(
    case: Case
 ): ScadObject {
+   return union {
+      backRotaryEncoderGearHolderLeftArm(case)
+      backRotaryEncoderGearHolderLeftArmSupportWall(case)
+   }
+}
+
+internal fun ScadParentObject.backRotaryEncoderGearHolderLeftArm(
+   case: Case
+): ScadObject {
    data class LeftArm(
       override val frontVector: Vector3d,
       override val bottomVector: Vector3d,
@@ -46,4 +55,43 @@ internal fun ScadParentObject.backRotaryEncoderGearHolder(
             Case.BACK_ROTARY_ENCODER_GEAR_HOLDER_ARM_WIDTH / 2)
       }
    }
+}
+
+internal fun ScadParentObject.backRotaryEncoderGearHolderLeftArmSupportWall(
+   case: Case
+): ScadObject {
+   val frontPlane = run {
+      val key = case.alphanumericPlate
+         .columns[Case.BACK_ROTARY_ENCODER_COLUMN_INDEX]
+         .keySwitches.first()
+
+      val keyPlane = Plane3d(key.referencePoint, key.topVector)
+
+      Plane3d(
+         keyPlane
+            intersection alphanumericBackSlopePlane(case.alphanumericPlate, offset = 0.mm)
+            intersection backRotaryEncoderGearPlane(case, offset = 0.mm),
+         case.frontVector
+      )
+   }
+
+   val topPlane = run {
+      val v = Vector3d(case.backRotaryEncoderMediationGear.referencePoint,
+                       case.backRotaryEncoderKnob.gearReferencePoint)
+      Plane3d(
+         case.backRotaryEncoderKnob.gearReferencePoint,
+         v vectorProduct backRotaryEncoderGearPlane(case, offset = 0.mm).normalVector
+      )
+   }
+
+   return distortedCube(
+      leftPlane = backRotaryEncoderGearPlane(
+         case, offset = PrinterAdjustments.minWallThickness.value),
+      rightPlane = backRotaryEncoderGearPlane(case, offset = 0.mm),
+      frontPlane = frontPlane,
+      backPlane = backRotaryEncoderCaseGearSideFrontPlane(
+         case.backRotaryEncoderGear.rotaryEncoder, offset = (-0.1).mm),
+      bottomPlane = alphanumericBackSlopePlane(case.alphanumericPlate, offset = (-0.1).mm),
+      topPlane = topPlane
+   )
 }
